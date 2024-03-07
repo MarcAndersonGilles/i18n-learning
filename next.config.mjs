c5ba673b3dec54694b4406275836c3ec0a504b3b
marc <hornil6748@gmail.com>
2024-03-07 15:46:12 -0500

    feature/add next js config and middleware for i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    i18n: {
+        // These are all the locales you want to support in
+        // your application
+        locales: ['default', 'fr', 'en', ],
+        defaultLocale: 'default',
+        // This is the default locale you want to be used when visiting
+        // a non-locale prefixed path e.g. `/hello`
+        localeDetection: false,
+      },
+      trailingSlash: true,
+};
 
 export default nextConfig;
